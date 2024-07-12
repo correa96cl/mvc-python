95a3521bb35114984888452f04c03858ac1c4ca8
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-07-11 22:06:10 -0300

    Commit

--- src/models/sqlite/settings/connection.py
+++ src/models/sqlite/settings/connection.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
     def __init__(self) -> None:
@@ -10,5 +11,13 @@ class DBConnectionHandler:
     
     def get_engine(self):
         return self.__engine
+    
+    def __enter__(self):
+        session_maker =sessionmaker()
+        self.session = session_maker(bind=self.__engine)
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+        
 
 db_connection_handler = DBConnectionHandler()
